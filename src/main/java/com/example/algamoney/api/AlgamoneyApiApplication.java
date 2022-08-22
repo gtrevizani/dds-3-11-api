21f7e140916a20a2b148938f43c3941a081d24cc
Guilherme I. Trevizani <99826402+gtrevizani@users.noreply.github.com>
2022-08-22 14:45:09 -0300

    Update AlgamoneyApiApplication.java

--- src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
+++ src/main/java/com/example/algamoney/api/AlgamoneyApiApplication.java
@@ -3,6 +3,9 @@ package com.example.algamoney.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Essa aplicação interage com Banco de Dados, fazendo o CRUD.
+// As mais importantes são as classes resource, que interagem diretamente com o banco de dados.
+
 @SpringBootApplication
 public class AlgamoneyApiApplication {
 
