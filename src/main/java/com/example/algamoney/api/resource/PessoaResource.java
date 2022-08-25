f789b3114cc9681244d70618771ba4eb57ec67f9
Guilherme I. Trevizani <guilhermetrevz@outlook.com>
2022-08-25 16:02:08 -0300

    Update PessoaResource.java

--- src/main/java/com/example/algamoney/api/resource/PessoaResource.java
+++ src/main/java/com/example/algamoney/api/resource/PessoaResource.java
@@ -54,6 +54,18 @@ public class PessoaResource {
 	public void remover(@PathVariable Long codigo) {
 		this.pessoaRepository.deleteById(codigo);
 	}
+	
+	@PutMapping("/{codigo}")
+	public ResponseEntity<Pessoa> atualizar(@PathVariable Long codigo, @Valid @RequestBody Pessoa pessoa) {
+		Pessoa pessoaSalva = pessoaService.atualizar(codigo, pessoa);
+		return ResponseEntity.ok(pessoaSalva);
+	}
+	
+	@PutMapping("/{codigo}/ativo")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizarPropriedadeAtivo(@PathVariable Long codigo, @RequestBody Boolean ativo) {
+		pessoaService.atualizarPropriedadeAtivo(codigo, ativo);
+	}
 
 	
 }
